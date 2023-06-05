b65d8e88867d616da33b54ddc29c7767e05e7420
sankirthi467 <124484889+sankirthi467@users.noreply.github.com>
2023-06-05 21:41:27 +0530

    performedfirst

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("welcome to my daily track progress page");
+var data1 = {image: "present.jpg",name:"K santoshi laxmi", designation:"undergraduate"};
+var data2 = {image:"childhood.jpg",name:"santoshi",designation: "developer"};
+var isdata1 = true;
+var displayData;
+var changepic =function(){
+    if(isdata1){
+        displayData=data2;
+        isdata1=false;
+    }
+    else{
+        displayData=data1;
+        isdata1=true;
+    }
+    document.getElementById("imageid").src=displaydata.image;
+}
